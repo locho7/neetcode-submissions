e1da15ab2e79d51277aaf2e3994b7d2003842872
Loc Ho <llocho2007@gmail.com>
2026-08-01 18:08:53 -0700

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return searchHelper(nums, target, 0, len(nums))
+
+def searchHelper(nums: List[int], target: int, lo: int, hi: int) -> int:
+    print(lo, hi)
+    if hi - lo == 1:
+        return lo if nums[lo] == target else -1
+    
+    med = lo + (hi - lo) // 2
+    if nums[med] == target:
+        return med
+    elif nums[med] > target:
+        return searchHelper(nums, target, lo, med)
+    elif nums[med] < target:
+        return searchHelper(nums, target, med, hi)
+
+    
