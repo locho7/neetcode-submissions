520a89dcf191943d7368000d82a24905ce487f9b
Loc Ho <llocho2007@gmail.com>
2026-08-19 17:47:49 -0700

    Add: implement-prefix-tree - submission-6

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-6.py
@@ -0,0 +1,33 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.children = {}
+        self.isWordEnd = False
+
+    def insert(self, word: str) -> None:
+        p = self
+        for char in word:
+            # char = char.lower()
+            if char not in p.children:
+                p.children[char] = PrefixTree()
+            p = p.children[char]
+        p.isWordEnd = True
+
+    def search(self, word: str) -> bool:
+        p = self
+        for char in word:
+            # char = char.lower()
+            if char not in p.children:
+                return False
+            p = p.children[char]
+        return p.isWordEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        p = self
+        for char in prefix:
+            # char = char.lower()
+            if char not in p.children:
+                return False
+            p = p.children[char]
+        return True
+        
