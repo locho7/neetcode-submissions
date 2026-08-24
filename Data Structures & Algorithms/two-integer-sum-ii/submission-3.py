fede5aec383f878fd44087e73bb08129bd51ec56
Loc Ho <llocho2007@gmail.com>
2026-08-24 08:20:21 -0700

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        lo = 0
+        hi = len(numbers) - 1
+        
+        while lo < hi:
+            dif = target - numbers[hi]
+            if dif == numbers[lo]:
+                return [lo + 1, hi + 1]
+            elif dif > numbers[lo]:
+                lo += 1
+            else:
+                hi -= 1
+
+
